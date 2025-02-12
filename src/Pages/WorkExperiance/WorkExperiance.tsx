99bd1ffc239c3161326697ccefb55a0a21a66757
Moeez Tofiq <moeeztofiq@gmail.com>
2025-02-12 19:35:11 +0500

    added pages and added paths

--- src/Pages/WorkExperiance/WorkExperiance.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const WorkExperiance = () => {
-  return <div>WorkExperiance</div>;
-};
-
-export default WorkExperiance;
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,35 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./Pages/ErrorPage/ErrorPage.tsx";
 import LandingPage from "./Pages/LandingPage/LandingPage.tsx";
+import Contact from "./Pages/Contact/Contact.tsx";
+import Credits from "./Pages/Credites/Credites.tsx";
+import PersonalProjects from "./Pages/PersonalProjects/PersonalProjects.tsx";
+import WorkExperience from "./Pages/WorkExperience/WorkExperience.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPage></LandingPage>,
+    errorElement: <ErrorPage></ErrorPage>,
+  },
+  {
+    path: "/contact",
+    element: <Contact></Contact>,
+    errorElement: <ErrorPage></ErrorPage>,
+  },
+  {
+    path: "/credits",
+    element: <Credits></Credits>,
+    errorElement: <ErrorPage></ErrorPage>,
+  },
+  {
+    path: "/personalProjects",
+    element: <PersonalProjects></PersonalProjects>,
+    errorElement: <ErrorPage></ErrorPage>,
+  },
+  {
+    path: "/workExperience",
+    element: <WorkExperience></WorkExperience>,
     errorElement: <ErrorPage></ErrorPage>,
   },
 ]);
