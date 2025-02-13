6aa8cba2c8fd32117b9512cee9d7d2ac4d13d8a4
Moeez Tofiq <moeeztofiq@gmail.com>
2025-02-13 18:49:05 +0500

    fine tuned animation

--- src/Components/Name.tsx
+++ src/Components/Name.tsx
@@ -4,10 +4,10 @@ import React, { useEffect, useState } from "react";
 const LightCircle: React.FC<{ isActive: boolean }> = ({ isActive }) => {
   return (
     <span
-      className={`h-[24px] w-[25px] bg-amber-200 rounded-4xl inline-block m-0.5 ${
+      className={`h-[24px] w-[25px] bg-amber-200 rounded-4xl inline-block m-0.5 transition-all duration-300 ${
         isActive
           ? "bg-amber-200 shadow-[0_0_8px_4px_rgba(255,191,71,0.4),0_0_8px_6px_rgba(255,191,71,0.6)]"
-          : "bg-amber-950"
+          : "bg-amber-950 shadow-none"
       }`}
     />
   );
