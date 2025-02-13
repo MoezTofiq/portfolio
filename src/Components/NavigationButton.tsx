6d5e22653f902a510ca7b5e9a35089bf6e339f50
Moeez Tofiq <moeeztofiq@gmail.com>
2025-02-13 22:01:40 +0500

    save

--- src/Components/NavigationButton.tsx
+++ src/Components/NavigationButton.tsx
@@ -4,7 +4,7 @@ const NavigationButton: React.FC<{ text: string; tilt: number }> = ({
 }) => {
   return (
     <div
-      className="z-10 text-3xl  font-medium text-shadow  transition-all duration-300 bg-amber-100 hover:bg-amber-200 rounded-2xl p-5"
+      className="z-10 text-3xl cursor-pointer font-medium text-shadow textUnderLine transition-all duration-300 bg-amber-100 hover:bg-amber-200 rounded-2xl p-5 hover:shadow-[0_0_8px_4px_rgba(255,191,71,0.4),0_0_8px_6px_rgba(255,191,71,0.6)]"
       style={{ transform: `rotate(${tilt}deg)` }}
     >
       <h2>{text}</h2>
--- src/Components/NavigationButtons.tsx
+++ src/Components/NavigationButtons.tsx
@@ -2,14 +2,11 @@ import NavigationButton from "./NavigationButton";
 
 const NavigationButtons = () => {
   return (
-    <div
-      className="flex flex-row justify-center items-center gap-36 rounded-3xl bg-amber-100 z-10 p-2 
-    shadow-[0_0_10px_8px_rgba(255,191,71,0.4),0_0_10px_8px_rgba(255,191,71,0.6)] hover:shadow-[0_0_20px_8px_rgba(255,191,71,0.4),0_0_20px_8px_rgba(255,191,71,0.6)]  transition-all duration-300"
-    >
-      <NavigationButton tilt={8} text="temp" />
-      <NavigationButton tilt={-8} text="temp" />
-      <NavigationButton tilt={8} text="temp" />
-      <NavigationButton tilt={8} text="temp" />
+    <div className="flex flex-row justify-center items-center gap-28 rounded-3xl bg-red-900 z-10 p-2 transition-all duration-300 shadow-[0_0_10px_8px_rgba(255,0,0,0.5),0_0_10px_rgba(255,0,0,0.7)]">
+      <NavigationButton tilt={15} text="About Me" />
+      <NavigationButton tilt={-15} text="Work Experience" />
+      <NavigationButton tilt={15} text="Personal Projects" />
+      <NavigationButton tilt={-15} text="Website Credit" />
     </div>
   );
 };
