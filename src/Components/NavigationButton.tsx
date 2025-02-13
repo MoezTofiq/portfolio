f5bd89b4e60a7e3836bb36c28ff4dbd8d3c84689
Moeez Tofiq <moeeztofiq@gmail.com>
2025-02-13 22:13:45 +0500

    finished nav bar

--- src/Components/NavigationButton.tsx
+++ src/Components/NavigationButton.tsx
@@ -4,10 +4,12 @@ const NavigationButton: React.FC<{ text: string; tilt: number }> = ({
 }) => {
   return (
     <div
-      className="z-10 text-3xl cursor-pointer font-medium text-shadow textUnderLine transition-all duration-300 bg-amber-100 hover:bg-amber-200 rounded-2xl p-5 hover:shadow-[0_0_8px_4px_rgba(255,191,71,0.4),0_0_8px_6px_rgba(255,191,71,0.6)]"
+      className="hover:bg-red-400 hover:shadow-[0_0_10px_8px_rgba(255,0,0,0.5),0_0_10px_rgba(255,0,0,0.7)] text-shadow transition-all duration-300 p-3 rounded-3xl"
       style={{ transform: `rotate(${tilt}deg)` }}
     >
-      <h2>{text}</h2>
+      <div className="z-10 text-3xl cursor-pointer font-medium text-shadow textUnderLine transition-all duration-300 bg-amber-100 hover:bg-amber-200 rounded-2xl p-5 hover:shadow-[0_0_8px_4px_rgba(255,191,71,0.4),0_0_8px_6px_rgba(255,191,71,0.6)]">
+        <h2>{text}</h2>
+      </div>
     </div>
   );
 };
--- src/Components/NavigationButtons.tsx
+++ src/Components/NavigationButtons.tsx
@@ -2,7 +2,7 @@ import NavigationButton from "./NavigationButton";
 
 const NavigationButtons = () => {
   return (
-    <div className="flex flex-row justify-center items-center gap-28 rounded-3xl bg-red-900 z-10 p-2 transition-all duration-300 shadow-[0_0_10px_8px_rgba(255,0,0,0.5),0_0_10px_rgba(255,0,0,0.7)]">
+    <div className="flex flex-row justify-center items-center gap-28 z-10  ">
       <NavigationButton tilt={15} text="About Me" />
       <NavigationButton tilt={-15} text="Work Experience" />
       <NavigationButton tilt={15} text="Personal Projects" />
