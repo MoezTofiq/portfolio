6cf2050f8456901089249f6f66baafff8c630136
MoezTofiq <moeeztofiq@gmailcom>
2025-02-14 16:09:39 +0500

    removed first render error for useEffect for nav buttons

--- src/Components/NavigationButton.tsx
+++ src/Components/NavigationButton.tsx
@@ -1,31 +1,9 @@
-// const NavigationButton: React.FC<{
-//   text: string;
-//   tilt: number;
-//   onClick: () => void;
-// }> = ({ text, tilt, onClick }) => {
-//   return (
-//     <div
-//       className="hover:bg-red-400 hover:shadow-[0_0_10px_8px_rgba(255,0,0,0.5),0_0_10px_rgba(255,0,0,0.7)] rotationCancel transition-all duration-300 p-3 rounded-3xl cursor-pointer"
-//       style={{ transform: `rotate(${tilt}deg)` }}
-//       onClick={onClick}
-//     >
-//       <div className="z-10 text-3xl cursor-pointer font-medium rotationCancel textUnderLine transition-all duration-300 bg-amber-100 hover:bg-amber-200 rounded-2xl p-5 hover:shadow-[0_0_8px_4px_rgba(255,191,71,0.4),0_0_8px_6px_rgba(255,191,71,0.6)]">
-//         <h2>{text}</h2>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default NavigationButton;
-
 const NavigationButton: React.FC<{
   text: string;
-  tilt: number;
   index: number;
   isSelected: boolean;
   setSelected: (index: number) => void;
 }> = ({ text, index, isSelected, setSelected }) => {
-  console.log(isSelected);
   return (
     <div
       className={`transition-all duration-300 flex flex-col  text-2xl ${
--- src/Components/NavigationButtons.tsx
+++ src/Components/NavigationButtons.tsx
@@ -1,53 +1,70 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavigationButton from "./NavigationButton";
 import { useNavigate } from "react-router-dom";
+
+const buttons = [
+  "Home",
+  "About Me",
+  "Work Experience",
+  "Personal Projects",
+  "Website Credit",
+  "Contact Me",
+];
 
 const NavigationButtons = () => {
   const navigate = useNavigate();
   const [selected, setSelected] = useState(1);
-  // onClick={() => navigate("/about_me")}
-  // onClick={() => navigate("/personal_projects")}
-  // onClick={() => navigate("/work_experience")}
-  // onClick={() => navigate("/credits")}
-  // onClick={() => navigate("/contact_me")}
+  const isFirstRender = useRef(true); //this is used to prevent the useEffect from running on the first render
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      console.log("first render");
+      return;
+    }
+
+    switch (selected) {
+      case 1:
+        navigate("/");
+        console.log("home");
+        break;
+      case 2:
+        navigate("/about_me");
+        console.log("about me");
+        break;
+      case 3:
+        navigate("/personal_projects");
+        console.log("personal projects");
+        break;
+      case 4:
+        navigate("/work_experience");
+        console.log("work experience");
+        break;
+      case 5:
+        navigate("/credits");
+        console.log("credits");
+        break;
+      case 6:
+        navigate("/contact_me");
+        console.log("contact me");
+        break;
+      default:
+        console.log("route not found");
+        break;
+    }
+  }, [selected]);
 
   return (
     <div className="flex flex-row justify-center items-center gap-5 z-10  pt-5 transition-all duration-300 p-10">
-      <NavigationButton
-        tilt={15}
-        text="About Me"
-        index={1}
-        isSelected={selected === 1}
-        setSelected={setSelected}
-      />
-      <NavigationButton
-        tilt={-15}
-        text="Work Experience"
-        index={2}
-        isSelected={selected === 2}
-        setSelected={setSelected}
-      />
-      <NavigationButton
-        tilt={15}
-        text="Personal Projects"
-        index={3}
-        isSelected={selected === 3}
-        setSelected={setSelected}
-      />
-      <NavigationButton
-        tilt={-15}
-        text="Website Credit"
-        index={4}
-        isSelected={selected === 4}
-        setSelected={setSelected}
-      />{" "}
-      <NavigationButton
-        tilt={15}
-        index={5}
-        isSelected={selected === 5}
-        setSelected={setSelected}
-        text="Contact Me"
-      />
+      {buttons.map((button, index) => (
+        <NavigationButton
+          key={index + 1}
+          text={button}
+          index={index}
+          isSelected={selected === index + 1}
+          setSelected={setSelected}
+        />
+      ))}
     </div>
   );
 };
