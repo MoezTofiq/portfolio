c25b21a5f93b905ca4a574a93945e2458202c7cb
Moeez Tofiq <moeeztofiq@gmail.com>
2025-02-17 18:56:15 +0500

    added current credites for the website

--- src/Pages/Credites/Credites.tsx
+++ src/Pages/Credites/Credites.tsx
@@ -3,18 +3,85 @@ const Credits = () => {
     <div className="bg-white shadow sm:rounded-lg sm:overflow-hidden m-3 p-2">
       <div className="px-4 sm:px-0">
         <h3 className="text-base/7 font-semibold text-gray-900">
-          Applicant Information
+          Website Technology & Ideas
         </h3>
         <p className="mt-1 max-w-2xl text-sm/6 text-gray-500">
-          Personal details and application.
+          Technology & styling Ideas used to create this portfolio.
         </p>
       </div>
       <div className="mt-6 border-t border-gray-100">
         <dl className="divide-y divide-gray-100">
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm/6 font-medium text-gray-900">Full name</dt>
+            <dt className="text-sm/6 font-medium text-gray-900">Technology</dt>
             <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-              Margot Foster
+              <li>
+                <a
+                  href="https://react.dev/"
+                  className=" textUnderLine transition-all duration-300"
+                >
+                  React
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://reactrouter.com/6.29.0"
+                  className=" textUnderLine transition-all duration-300"
+                >
+                  React Router Dom V6.29.0
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://tailwindcss.com/"
+                  className=" textUnderLine transition-all duration-300"
+                >
+                  Tailwind V4.0
+                </a>
+              </li>
+            </dd>
+          </div>
+        </dl>
+      </div>
+      <div className="mt-6 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm/6 font-medium text-gray-900">
+              Design Ideas
+            </dt>
+            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <li>
+                <a
+                  href="https://www.wehackutd.com/"
+                  className=" textUnderLine transition-all duration-300"
+                >
+                  WeHack Landing page (used for color pallet and over all feel)
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.microsoft.com/en-us/edge/welcome"
+                  className=" textUnderLine transition-all duration-300"
+                >
+                  MicroSoft Edge Welcome page (feel and animation)
+                </a>
+              </li>
+            </dd>
+          </div>
+        </dl>
+      </div>
+      <div className="mt-6 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm/6 font-medium text-gray-900">Assets used</dt>
+            <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <li>
+                <a
+                  href="https://www.onlinewebfonts.com/icon/422506"
+                  className=" textUnderLine transition-all duration-300"
+                >
+                  About Me Icon
+                </a>
+              </li>
             </dd>
           </div>
         </dl>
